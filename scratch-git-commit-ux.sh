d074501752126b06555dad35c85f6f0cd7aea2f0
progamer6918 <progamer6918@users.noreply.github.com>
2026-06-13 23:30:40 +0700

    chore: remove scratch-git-commit-ux.sh

--- scratch-git-commit-ux.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd /home/egitaristorandas/AI_WORKSPACES/airo-second-brain
-
-git add .
-git commit -m "fix(airo-brain): add post-detail Earesmes decision buttons"
-git push
